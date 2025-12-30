244394eaf22ae4b16c3abd89542a57c18ae4b196
jyothsna30-design <jyothsnapriyanka30@gmail.com>
2025-12-30 16:05:16 +0300

    server comments added

--- server.js
+++ server.js
@@ -7,17 +7,17 @@ import authrouter from "./routes/authRoutes.js";
 
 const app = express();
 app.use(express.json());
-
+//starting server with port 8080
 app.listen("8080", ()=>{console.log("Server is running at port 8080")});
-
+//defining routes
 app.use("/products", productrouter);
 app.use("/cart", cartrouter);
 app.use("/", authrouter);
 
-/* Global Error Handler (optional but recommended) */
+//middleware to handle any errors
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ message: "Something went wrong!" });
 });
-
+//connection to database
 connectDB();
